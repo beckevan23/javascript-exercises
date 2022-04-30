01994351f0936748b05e47523037a08e2c67716b
nefarox <evan.beck23@gmail.com>
2022-04-30 09:38:47 -0500

    Finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let newString = string;
+    if (number == 0) {
+        return "";
+    } else if (number < 0) {
+        return "ERROR";
+    };
+    for (let i = 1; i < number; ++i){
+        newString += string;
+    } return newString;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
