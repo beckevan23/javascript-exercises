ad4aa34edda8675706d12d15a0217b0162d0cc96
nefarox <evan.beck23@gmail.com>
2022-05-01 11:25:40 -0500

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, num1, num2, num3, num4) {
+    for (let i = array.length -1; i >= 0; i--) {
+        if (array[i] === num1) {
+            array.splice(i, 1);
+        } else if (array[i] === num2) {
+            array.splice(i, 1);
+        } else if (array[i] === num3) {
+            array.splice(i, 1);
+        } else if (array[i] === num4) {
+            array.splice(i, 1);
+        };
+    };
+    return array;
 };
 
 // Do not edit below this line
