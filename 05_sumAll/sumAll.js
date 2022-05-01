87ac953d96aa4463871a57b17ae984a3ea17e04b
nefarox <evan.beck23@gmail.com>
2022-05-01 11:54:41 -0500

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(one, two) {
+    let finalSum = 0;
+    if (one < 0 || two < 0 || typeof one != "number" || typeof two != "number") {
+        return "ERROR";
+    } else if (one > 0 && two > 0 && one < two) {
+        for (let i = one; i < two + 1; ++i) {
+           finalSum += i;
+        } return finalSum;  
+    } else if (one > 0 && two > 0 && one > two) {
+        for (i = two; i < one + 1; ++i) {
+            finalSum += i;
+        } return finalSum;
+    };
 };
 
 // Do not edit below this line
