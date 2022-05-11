0286c010c8fa6e602e391d8ec3b4e798e9ec501f
nefarox <evan.beck23@gmail.com>
2022-05-11 13:58:52 -0500

    Finish fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let n1 = 0, n2 = 1, nextTerm;
+if (n > 0) {
+    for (let i = 1; i < n; i++) {
+        nextTerm = n1 + n2;
+        n1 = n2;
+        n2 = nextTerm;
+    } return n2;
+} else return "OOPS";
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
