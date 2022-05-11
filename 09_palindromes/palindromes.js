6ba17458cc1dc8e3fa3716bb04f38e84e33dcd7c
nefarox <evan.beck23@gmail.com>
2022-05-10 20:40:59 -0500

    Finish palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newString = string.replace(/[^\w\']|_/g, "")
+                          .replace(/\s+/g, " "); 
+    let array = newString.split('');
+    let reversed = array.reverse();
+    let joined = reversed.join('');
+    return newString.toLowerCase() == joined.toLowerCase() ? true : false;    
 };
 
 // Do not edit below this line
