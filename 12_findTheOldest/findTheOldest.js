7acedc57c16ed86cc6944b9798167624a9939b3f
nefarox <evan.beck23@gmail.com>
2022-05-11 21:33:11 -0500

    FINISHED FUCKING findTheOldestgit add .git add .

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    for (let object in array) {
+        if (!array[object].yearOfDeath) {
+            array[object].yearOfDeath = new Date().getFullYear();
+        }
+    }
+    const oldest = array.sort(function(a, b) {
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = b.yearOfDeath - b.yearOfBirth;
+        return lastGuy > nextGuy ? -1 : 1;
+    })
+    return oldest[0]
 };
 
 // Do not edit below this line
